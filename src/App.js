89b2e63c538104722bea8d5e44d71979d5709573
vijay <VijayIyer>
2023-07-01 11:03:39 -0400

    Refactors userAuthorized useEffect

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ import { TicTacToe } from "./components/TicTacToe";
 import { GameContextProvider } from "./components/Connect4/gameContextProvider";
 import { NavBarHeader } from "./components/NavBar";
 import { Connect4 } from "./components/Connect4/index.jsx";
-import UnauthorizedUser from './components/UnauthorizedUser';
+import UnauthorizedUser from "./components/UnauthorizedUser";
 import "./App.css";
 import { appContext } from "./AppContext";
-import { socket } from './socket';
+import { socket } from "./socket";
 import { useEffect, useState } from "react";
 
 function App() {
@@ -20,20 +20,19 @@ function App() {
   const [userName, setUserName] = useState(null);
   const [authorized, setAuthorized] = useState(true);
   const navigate = useNavigate();
-  const createNewGame = (type, turn, callBack)=>{
-    socket.emit('createNewGame', {token: user, type: type, turn:turn}, callBack)
-  }
-  const joinGame = (type, turn, id, callBack)=>{
+  const createNewGame = (type, turn) => {
+    socket.emit("createNewGame", { token: user, type: type, turn: turn });
+  };
+  const joinGame = (type, turn, id) => {
     console.log(`joining game with turn ${turn}`);
-    socket.emit('joinGame', { token: user, id:id, turn:turn }, (data)=>{
-      callBack();
+    socket.emit("joinGame", { token: user, id: id, turn: turn }, (data) => {
       let route;
       console.log(data);
-      switch(type){
-        case 'Connect4':
+      switch (type) {
+        case "Connect4":
           route = `/game/${type}/${id}`;
           break;
-        case 'TicTacToe':
+        case "TicTacToe":
           route = `/game/${type}/${id}`;
           break;
         default:
@@ -41,33 +40,31 @@ function App() {
           break;
       }
       console.log(route);
-      if(route){
-        navigate(route, { state : data });  
+      if (route) {
+        navigate(route, { state: data });
       }
-      
     });
-  }
-  const handleTokenError = () => setAuthorized(false);
+  };
+
   useEffect(() => {
+    const handleTokenError = () => setAuthorized(false);
     socket.on("userUnauthorized", handleTokenError);
-    return ()=>{
+    return () => {
       socket.off("userUnauthorized", handleTokenError);
-    }
+    };
   }, [user]);
-  useEffect(()=>{
-    socket.on('errorCreatingNewGame', (data)=>{
+  useEffect(() => {
+    function printError(data) {
       console.log(data);
-      return ()=>{
-        socket.off('errorCreatingNewGame', (data)=>{
-          console.log(data);
-        })
-      }
-    })
-  })
-  
+    }
+    socket.on("errorCreatingNewGame", (data) => printError(data));
+    return () => {
+      socket.off("errorCreatingNewGame", (data) => printError(data));
+    };
+  });
+
   return (
     <>
-      
       <div className='App'>
         <appContext.Provider
           value={{
@@ -76,44 +73,40 @@ function App() {
             socket,
             userName,
             setUserName,
-            authorized, 
-            setAuthorized, 
+            authorized,
+            setAuthorized,
             createNewGame,
-            joinGame
+            joinGame,
           }}
         >
           <Container fluid>
-            
-              
-              <NavBarHeader />
-              <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/login' element={<Login />} />
-                <Route path='/register' element={<Registration />} />
-                <Route path='/newGame' element={<ChooseGame />} />
-                <Route path='/game/TicTacToe/:id' element={<TicTacToe />} />
-                <Route path='/game/TicTacToe' element={<TicTacToe />} />
-                <Route
-                  path='/game/Connect4/:id'
-                  element={
-                    <GameContextProvider>
-                      <Connect4 numRows={8} numCols={6} />
-                    </GameContextProvider>
-                  }
-                />
-                <Route
-                  path='/game/Connect4'
-                  element={
-                    <GameContextProvider>
-                      <Connect4 numRows={8} numCols={6} />
-                    </GameContextProvider>
-                  }
-                />
-                <Route path='*' element={<PageNotFound />} />
-              </Routes>
-            <UnauthorizedUser authorized={authorized}/>
-            
-            
+            <NavBarHeader />
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Registration />} />
+              <Route path='/newGame' element={<ChooseGame />} />
+              <Route path='/game/TicTacToe/:id' element={<TicTacToe />} />
+              <Route path='/game/TicTacToe' element={<TicTacToe />} />
+              <Route
+                path='/game/Connect4/:id'
+                element={
+                  <GameContextProvider>
+                    <Connect4 numRows={8} numCols={6} />
+                  </GameContextProvider>
+                }
+              />
+              <Route
+                path='/game/Connect4'
+                element={
+                  <GameContextProvider>
+                    <Connect4 numRows={8} numCols={6} />
+                  </GameContextProvider>
+                }
+              />
+              <Route path='*' element={<PageNotFound />} />
+            </Routes>
+            <UnauthorizedUser authorized={authorized} />
           </Container>
         </appContext.Provider>
       </div>
--- src/components/GameOptions/index.jsx
+++ src/components/GameOptions/index.jsx
@@ -1,41 +1,46 @@
-import TicTacToeOptions from './TicTacToeOptions';
-import Connect4Options from './Connect4Options';
-import { Container, Modal, Button } from 'react-bootstrap';
-import { appContext } from '../../AppContext';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState, useEffect, useContext } from 'react';
-function GameOptions({ type, handlePrompt}){
-	const { createNewGame } = useContext(appContext);
-	const [component, setComponent] = useState(null);
-	const handleChosenOption = (option)=>{
-			createNewGame(type, option, handlePrompt);
-	}
-	useEffect(()=>{
-		console.log(`setting show to ${type}`);
-		switch(type){
-			case 'TicTacToe':
-				setComponent(<TicTacToeOptions handleChosenOption={handleChosenOption} />);
-				break;
-			case 'Connect4':
-				setComponent(<Connect4Options handleChosenOption={handleChosenOption} />);
-				break;
-			default:
-				break;
-		}
-	}, [type]);
-	
-	return (
-		<Modal onHide={handlePrompt} show={type}>
-			<Modal.Dialog>
-	        {component}
+import TicTacToeOptions from "./TicTacToeOptions";
+import Connect4Options from "./Connect4Options";
+import { Container, Modal, Button } from "react-bootstrap";
+import { appContext } from "../../AppContext";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState, useEffect, useContext } from "react";
+function GameOptions({ type, handlePrompt }) {
+  const { createNewGame } = useContext(appContext);
+  const [component, setComponent] = useState(null);
+  const handleChosenOption = (option) => {
+    createNewGame(type, option);
+    handlePrompt();
+  };
+  useEffect(() => {
+    console.log(`setting show to ${type}`);
+    switch (type) {
+      case "TicTacToe":
+        setComponent(
+          <TicTacToeOptions handleChosenOption={handleChosenOption} />
+        );
+        break;
+      case "Connect4":
+        setComponent(
+          <Connect4Options handleChosenOption={handleChosenOption} />
+        );
+        break;
+      default:
+        break;
+    }
+  }, [type]);
 
-	        <Modal.Footer>
-	          <Button onClick={handlePrompt} variant="outline-dark">
-	          	Close
-	         	</Button>
-	        </Modal.Footer>
-	      </Modal.Dialog>
+  return (
+    <Modal onHide={handlePrompt} show={type}>
+      <Modal.Dialog>
+        {component}
+
+        <Modal.Footer>
+          <Button onClick={handlePrompt} variant='outline-dark'>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal.Dialog>
     </Modal>
-		)
+  );
 }
-export { GameOptions }
+export { GameOptions };
